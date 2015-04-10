e4cb52ccd0eef7d0c27e0fe2a726256963680d1b
karl <360512239@qq.com>
2015-04-10 17:33:04 +0800

    Initial some folders and files.

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ company show template
 # notes
 
 - Box is limited by number 6.
-- Menu\'s selected by css class m-selected
+- Menu's selected by css class m-selected
--- js/index.js
+++ js/index.js
@@ -164,30 +164,124 @@ $(function () {
 	};
 	_.renderBox($('.f-links-sponsor'), dataLayer5, '100%', 'sponsor');
 
+    var dataModule = {
+        'boxH': '公告栏',
+        'boxLink': 'http://luolinjia.com',
+        'isMore': true,
+        'list': [{
+            'link': 'http://baidu.com',
+            'title': '祝贺第五届最受关注十大评选颁奖盛典圆满落a啊啊啊',
+            'date': '2015-04-08'
+        },{
+            'link': 'http://baidu.com',
+            'title': '转发：浙江省商务厅什么等等我转发：浙江省商务厅什么等等我',
+            'date': '2015-04-07'
+        },{
+            'link': 'http://baidu.com',
+            'title': '电商先行，法律随后 ——记杭州市电子商务a厅什么等',
+            'date': '2015-04-06'
+        },{
+            'link': 'http://baidu.com',
+            'title': '电商先行，法律随后 ——记杭州市电子商务a厅什么等',
+            'date': '2015-04-06'
+        },{
+            'link': 'http://baidu.com',
+            'title': '电商先行，法律随后 ——记杭州市电子商务a厅什么等',
+            'date': '2015-04-06'
+        },{
+            'link': 'http://baidu.com',
+            'title': '电商先行，法律随后 ——记杭州市电子商务a厅什么等',
+            'date': '2015-04-06'
+        }]
+    };
+    _.renderBox($('.c-m-left-up'), dataModule, '240px', 'module');
 
+    var dataDirect = {
+        'boxH': '快捷通道',
+        'isMore': false,
+        'picList': [{
+                'link': 'http://luolinjia.com',
+                'img': 'images/mld1.jpg'
+            }, {
+                'link': 'http://luolinjia.com',
+                'img': 'images/mld2.jpg'
+            }]
+    };
+    _.renderBox($('.c-m-left-down'), dataDirect, '240px', 'direct');
+
+    // module news list
+    var newsList = {
+        'moduleH': '其它信息',
+        'list': [{
+            'link': 'http://baidu.com',
+            'title': '祝贺第五届最受关注十大评选颁奖盛典圆满落a啊啊啊',
+            'date': '2015-04-08'
+        },{
+            'link': 'http://baidu.com',
+            'title': '转发：浙江省商务厅什么等等我转发：浙江省商务厅什么等等我',
+            'date': '2015-04-07'
+        },{
+            'link': 'http://baidu.com',
+            'title': '电商先行，法律随后 ——记杭州市电子商务a厅什么等',
+            'date': '2015-04-06'
+        },{
+            'link': 'http://baidu.com',
+            'title': '电商先行，法律随后 ——记杭州市电子商务a厅什么等',
+            'date': '2015-04-06'
+        },{
+            'link': 'http://baidu.com',
+            'title': '电商先行，法律随后 ——记杭州市电子商务a厅什么等',
+            'date': '2015-04-06'
+        },{
+            'link': 'http://baidu.com',
+            'title': '电商先行，法律随后 ——记杭州市电子商务a厅什么等',
+            'date': '2015-04-06'
+        },{
+            'link': 'http://baidu.com',
+            'title': '电商先行，法律随后 ——记杭州市电子商务a厅什么等',
+            'date': '2015-04-06'
+        },{
+            'link': 'http://baidu.com',
+            'title': '电商先行，法律随后 ——记杭州市电子商务a厅什么等',
+            'date': '2015-04-06'
+        },{
+            'link': 'http://baidu.com',
+            'title': '电商先行，法律随后 ——记杭州市电子商务a厅什么等',
+            'date': '2015-04-06'
+        },{
+            'link': 'http://baidu.com',
+            'title': '电商先行，法律随后 ——记杭州市电子商务a厅什么等',
+            'date': '2015-04-06'
+        }]
+    };
+    _.renderModulePostList($('.c-m-content-right'), newsList);
 });
 
 var _ = {
     renderBox: function (self, data, width, flag) {
 		var innerDom = '';
 		if ('post' === flag) {
-			innerDom = _.renderPostList(data).join('');
+			innerDom = _.renderPostList(data, false).join('');
 		} else if ('about' === flag) {
 			innerDom = _.renderAbout(data);
 		} else if ('sponsor' === flag) {
 			innerDom = _.renderSponsors(data).join('');
-		}
+		} else if ('module' === flag) {
+            innerDom = _.renderPostList(data, true).join('');
+        } else if ('direct' === flag) {
+            innerDom = _.renderDirectPass(data).join('');
+        }
 
         var dom = '<div class="b-box"><div class="b-header"><span>' + data['boxH'] + '</span>' + (data['isMore'] ? '<a href="' + data['boxLink'] + '" target="_self"><img src="images/more.gif" alt=""/></a>' : '') + '</div><div class="b-list"><div class="b-list-content">' + innerDom + '</div></div></div>';
         self.append(dom);
         $('.b-box', self).css({'width': width});
     },
-    renderPostList: function (data) {
+    renderPostList: function (data, isModule) {
 		var list = [], i = 0, size = data['list'].length;
 		list.push('<ul>');
 		for (; i < size; i++) {
 			var item = data['list'][i];
-			list.push('<li><div class="b-list-content-title"><a href="' + item['link'] + '" target="_self"><span><img src="images/ico.jpg" alt=""/></span>'  + item['title'] + '</a></div><div class="b-list-content-time">' + item['date'] + '</div></li>');
+			list.push('<li><div class="b-list-content-title"><a href="' + item['link'] + '" target="_self"><span><img src="images/ico.jpg" alt=""/></span>'  + item['title'] + '</a></div>' + (isModule ? '' : '<div class="b-list-content-time">' + item['date'] + '</div>') + '</li>');
 		}
 		list.push('</ul>');
 		return list;
@@ -198,7 +292,7 @@ var _ = {
     renderPicBox: function (o) {
         o.picScroll({
             size: {'height': '270px', width: '530px'},
-            color: '#ddd', //rgba(221, 221, 221, 0.4)
+            color: '#ddd', //rgba(221, 221, 221, 0.4) it doesn't work in IE8-
             hovercolor: '#C70909',
             time: 2000,
             hasNumber: false
@@ -223,5 +317,25 @@ var _ = {
 		}
 		list.push('</ul>');
 		return list;
+    },
+    renderDirectPass: function (data) {
+        var list = [], i = 0, size = data['picList'].length;
+        for (; i < size; i++) {
+            var item = data['picList'][i];
+            list.push('<div class="m5"><a href="' + item['link'] + '"><img src="' + item['img'] + '" alt=""/></a></div>');
+        }
+        return list;
+    },
+    renderModulePostList: function (self, data) {
+        var list = [], i = 0, size = data['list'].length;
+        list.push('<ul>');
+        for (; i < size; i++) {
+            var item = data['list'][i];
+            list.push('<li><div class="m-list-content-title"><a href="' + item['link'] + '" target="_self"><span><img src="images/list_li.gif" alt=""/></span>'  + item['title'] + '</a></div><div class="m-list-content-time">' + item['date'] + '</div></li>');
+        }
+        list.push('</ul>');
+
+        var dom = '<div class="m-box"><div class="m-header"><img src="images/list_ico.gif" /><span>' + data['moduleH'] + '</span></div><div class="m-list"><div class="m-list-content">' + list.join('') + '</div></div></div>';
+        self.append(dom);
     }
 };
